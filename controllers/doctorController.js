dc45b33519eda66b3156b90c4b0f31b8b0d1af47
sai shashank <saishashank.pendyala@gmail.com>
2025-07-25 22:43:28 +0530

    changed

--- controllers/doctorController.js
+++ controllers/doctorController.js
@@ -22,9 +22,7 @@ const createSendToken = (user, statusCode, res) => {
     httpOnly: true,
   };
 
-  if (process.env.NODE_ENV === "production") {
-    cookieOptions.secure = true;
-  }
+  
 
   res.cookie("jwt", token, cookieOptions);
 
